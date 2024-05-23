627003e133a46755d5105e16ea50ff42d2af98d9
marinodilauro <m.dilauro90@gmail.com>
2024-05-23 14:40:20 +0200

    Add models relationship methods

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $guarded;
+
+    /**
+     * Get the type of the project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- database/migrations/2024_05_23_123044_add_type_id_foreign_key_to_projects_table.php
+++ database/migrations/2024_05_23_123044_add_type_id_foreign_key_to_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigIntegers('type_id')->nullable()->after('id');
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
 
             $table->foreign('type_id')
                 ->references('id')
