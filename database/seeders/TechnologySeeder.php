dcb894b6fd599393373aee567864966cd43e9099
marinodilauro <m.dilauro90@gmail.com>
2024-05-27 12:35:27 +0200

    Run seeder for technologies table

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = ['HTML', 'CSS', 'PHP', 'C#', 'Vue.js', 'JavaScript', 'Laravel', 'Vite', 'AI', 'Machine Learning', 'Game', 'Unity'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($newTechnology->name, '-');
+            $newTechnology->save();
+        }
     }
 }
