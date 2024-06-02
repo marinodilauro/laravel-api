aaaaea48d61e7d7b0ed6594a370ffad400dfdd7f
marinodilauro <m.dilauro90@gmail.com>
2024-06-02 21:04:32 +0200

    Fix create project form. Change projects per page.

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->orderByDesc('id')->paginate(8);
+        $projects = Project::with('type', 'technologies')->orderByDesc('id')->paginate(9);
         return response()->json([
             'success' => true,
             'projects' => $projects
--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -40,18 +40,6 @@
       </div>
 
       <div class="mb-3">
-        <label for="title" class="form-label">Title</label>
-        <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
-          aria-describedby="titleHelper" placeholder="Food Delivery App" value="{{ old('title') }}" />
-        <small id="titleHelper" class="form-text text-muted">Type a title for this project</small>
-
-        @error('title')
-          <div class="text-danger">{{ $message }}</div>
-        @enderror
-
-      </div>
-
-      <div class="mb-3">
         <label for="type_id" class="form-label">Type</label>
         <select class="form-select" name="type_id" id="type_id">
           <option selected disabled>Select a type for the project</option>
@@ -122,8 +110,7 @@
           <div class="mb-3">
             <label for="repo_link" class="form-label">Repository link</label>
             <input type="text" class="form-control @error('repo_link') is-invalid @enderror" name="repo_link"
-              id="repo_link" aria-describedby="repo_linkHelper" placeholder="https://"
-              value="{{ old('repo_link') }}" />
+              id="repo_link" aria-describedby="repo_linkHelper" placeholder="https://" value="{{ old('repo_link') }}" />
             <small id="repo_linkHelper" class="form-text text-muted">Insert the link of the repository</small>
 
             @error('repo_link')
