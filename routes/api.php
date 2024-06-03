6d217eaf4d8da217d2145122ca937cb4fe7e21fc
marinodilauro <m.dilauro90@gmail.com>
2024-06-03 10:58:31 +0200

    Add route for single project page in frontend

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('projects', [ProjectController::class, 'index']);
+Route::get('projects/{project:slug}', [ProjectController::class, 'show']);
 Route::get('main', [ProjectController::class, 'main']);
